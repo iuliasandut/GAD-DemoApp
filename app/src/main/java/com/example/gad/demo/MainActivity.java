0b9cd41ba6138d3c634353b57016603dc1ea145f
Madalin Gageanu <madalin.gageanu@gmail.com>
2020-05-06 21:31:00 +0300

    Added WebView implementation.

--- app/src/main/java/com/example/gad/demo/MainActivity.java
+++ app/src/main/java/com/example/gad/demo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gad.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView browser = findViewById(R.id.actMain_wvBrowser);
+
+        // enable javascript
+        WebSettings settings = browser.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        // if we wish to open the webpage in place, we need to specify a client that will render the page
+        browser.setWebViewClient(new MyWebClient());
+
+        browser.loadUrl("https://www.polygon.com");
+    }
+
+    public class MyWebClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // specifies that we should load the page in the webview we have and not an external browser window
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
