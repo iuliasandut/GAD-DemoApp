fff9de8a8b8662cb2af80e76c1b44c60a3b8fa9a
Madalin Gageanu <madalin.gageanu@gmail.com>
2020-05-06 21:02:13 +0300

    Added spinner simple implementation.

--- app/src/main/java/com/example/gad/demo/MainActivity.java
+++ app/src/main/java/com/example/gad/demo/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.gad.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import static android.R.layout.simple_spinner_item;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] data_source = {"Timisoara", "Cluj", "Iasi", "Bucuresti"};
+
+        // get a reference to the spinner view declared in layout xml
+        Spinner spinner = findViewById(R.id.actMain_ddCities);
+
+        // create a basic array adapter and specify the layout for the item
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, simple_spinner_item, data_source);
+
+        // specify the layout for the dropdown list items
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
+
 }
