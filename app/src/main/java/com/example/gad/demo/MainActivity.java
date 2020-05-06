fe86a274a74af24de592f60c7c5a29259a6fb31d
Madalin Gageanu <madalin.gageanu@gmail.com>
2020-05-06 21:19:42 +0300

    Added listener implementation.

--- app/src/main/java/com/example/gad/demo/MainActivity.java
+++ app/src/main/java/com/example/gad/demo/MainActivity.java
@@ -7,30 +7,84 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import static android.R.layout.simple_spinner_item;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
+
+    private static final String[] DATA_SOURCE = {"Timisoara", "Cluj", "Iasi", "Bucuresti"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] data_source = {"Timisoara", "Cluj", "Iasi", "Bucuresti"};
-
         // get a reference to the spinner view declared in layout xml
         Spinner spinner = findViewById(R.id.actMain_ddCities);
 
         // create a basic array adapter and specify the layout for the item
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, simple_spinner_item, data_source);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, simple_spinner_item, DATA_SOURCE);
 
         // specify the layout for the dropdown list items
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        // option 1 -> setting the listener using the interface implementation on the current class
+        // spinner.setOnItemSelectedListener(this);
+
+        // option 2 -> setting the listener using an inner class
+        // option 2.5 -> you may externalise inner class to it's own file
+        // spinner.setOnItemSelectedListener(new MyInnerSpinnerListener());
+
+        // option 3 -> setting the listener as an anonymous implementation of the interface
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tvSelectedCity = findViewById(R.id.actMain_tvSelectedCity);
+                String selected_city = DATA_SOURCE[position];
+
+                tvSelectedCity.setText(selected_city);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {}
+        });
     }
 
+
+    /* Methods below implement the interface AdapterView.OnItemSelectedListener */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView tvSelectedCity = findViewById(R.id.actMain_tvSelectedCity);
+        String selected_city = DATA_SOURCE[position];
+
+        tvSelectedCity.setText(selected_city);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {}
+
+
+    /* Inner class implementing the interface AdapterView.OnItemSelectedListener */
+    public class MyInnerSpinnerListener implements AdapterView.OnItemSelectedListener {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            TextView tvSelectedCity = findViewById(R.id.actMain_tvSelectedCity);
+            String selected_city = DATA_SOURCE[position];
+
+            tvSelectedCity.setText(selected_city);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent)
+        {}
+    }
 
 }
